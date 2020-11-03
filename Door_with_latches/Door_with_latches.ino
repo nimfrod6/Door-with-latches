da8db106164c5c7d0dd83f35a97fda5890a7c96b
LAPTOP\Nimfrodian <aljazpflaum@gmail.com>
2020-11-03 17:13:09 +0100

    End of day commit

--- Door_with_latches/Door_with_latches.ino
+++ Door_with_latches/Door_with_latches.ino
@@ -1,3 +1,20 @@
+#include "LiquidCrystal_I2C.h"
+#include "Keypad.h"
+
+const int ROW_NUM = 4; // four rows
+const int COLUMN_NUM = 4; // four columns
+
+char keys[ROW_NUM][COLUMN_NUM] = {
+  {'1','2','3', 'A'},
+  {'4','5','6', 'B'},
+  {'7','8','9', 'C'},
+  {'*','0','#', 'D'}
+};
+
+byte pin_rows[ROW_NUM] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte pin_column[COLUMN_NUM] = {5, 4, 3}; //connect to the column pinouts of the keypad
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
