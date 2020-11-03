ff5c8f5d37cacfe7bf3d2d3242590d8cf4336eb5
LAPTOP\Nimfrodian <aljazpflaum@gmail.com>
2020-11-03 21:11:58 +0100

    Added LCD support with LCD display timeoff

--- Door_with_latches/Door_with_latches.ino
+++ Door_with_latches/Door_with_latches.ino
@@ -1,5 +1,6 @@
 #include "LiquidCrystal_I2C.h"
 #include "Keypad.h"
+#include "Wire.h"
 
 typedef enum{
   LOCKED = 0,
@@ -19,9 +20,10 @@ char keys[ROW_NUM][COLUMN_NUM] = {
 
 byte pin_rows[ROW_NUM] = {6, 7, 8, 9}; //connect to the row pinouts of the keypad
 byte pin_column[COLUMN_NUM] = {2, 3, 4, 5}; //connect to the column pinouts of the keypad
-Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
 /******* END_KEYPAD ***************************/
 /******* GLOBAL_VARIABLES *********************/
+Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
 lockStatusType _lockStatus = LOCKED;
 
 const unsigned long UNLOCK_ON_TIME = 3000;
@@ -29,12 +31,16 @@ const unsigned long UNLOCK_LATCHES_MAX_TIME = 10000;
 const unsigned long AUTO_LOCK_TIME_RESET = 10000;      // resets unlock time back to 10 seconds
 const unsigned long AUTO_LOCK_TIME_INCREMENT = 300000; // prolongs unlock time by 5 minutes
 const unsigned long AUTO_LOCK_TIME_MAX = 7200000; // max 2 hours unlock time
+const unsigned long AUTO_DISPLAY_OFF_TIME = 60000;
 unsigned long AUTO_LOCK_TIME = 10000;
 unsigned long _lastUnlockTime = 0;
+unsigned long _lastKeyPressTime = 0;
 
 
 char _passCode[] = {'1', '8', '0', '9'};
 char _enteredCode[4] = {0};
+
+int remainingTime[3] = {0, 0, 0}; //hh mm ss
 
 int _enteredCodePosition = 0;
 
@@ -51,11 +57,24 @@ void setup() {
   digitalWrite( _latchGatePin, 0 );
   pinMode( _insideSwitchPin, INPUT );
   Serial.begin(9600);
+  Wire.begin();
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("  VHOD V REZIDENCO  ");
+  lcd.setCursor(0,1);
+  lcd.print("-----ZAKLENJENO-----");
+  lcd.setCursor(0,3);
+  lcd.print("Geslo: ");
 }
 /******* END_SETUP() **************************/
 /******* MAIN() *******************************/
 void loop() {
   char key = keypad.getKey();
+  if( key )
+  {
+    _lastKeyPressTime = millis();
+  }
   switch(_lockStatus)
   {
     
@@ -107,6 +126,7 @@ void loop() {
   UnlockLatches();
   digitalWrite( 13, _lockStatus);
   AutoLock();
+  printLCD();
   if( Serial )
   {
     debug();
@@ -197,13 +217,15 @@ bool PowerLimit( bool condition)
   return stateToReturn;
 }
 
-void AutoLock() // locks the door after certain time has passed
+unsigned long AutoLock() // locks the door after certain time has passed
 {
-  if( millis() - _lastUnlockTime > AUTO_LOCK_TIME )
+  unsigned long passedTime = millis() - _lastUnlockTime;
+  if( passedTime > AUTO_LOCK_TIME )
   {
     _lockStatus = LOCKED;
     AUTO_LOCK_TIME = AUTO_LOCK_TIME_RESET;
   }
+  return AUTO_LOCK_TIME - passedTime;
 }
 
 void debug()
@@ -224,4 +246,90 @@ void debug()
 void LOG(String parameter, String parameterName)
 {
   Serial.print(parameterName + "= " + parameter + "\t");
+}
+
+void printLCD()
+{
+  static bool _lockStatusToggle = 0;
+  static bool cleared = 0;
+  static int _enteredCodePositionPrev = 0;
+  static double long statusChangeTime = 0;
+
+  if( _lockStatus == LOCKED )
+  {
+    if(_lockStatusToggle != _lockStatus)
+    {
+      statusChangeTime = millis();
+      lcd.setCursor(0, 0);
+      lcd.print("  VHOD V REZIDENCO  ");
+      lcd.setCursor(0,1);
+      lcd.print("-----ZAKLENJENO-----");
+      lcd.setCursor(0,3);
+      lcd.print("Geslo:              ");
+      lcd.setCursor(7,3);
+    }
+    if(_enteredCodePositionPrev != _enteredCodePosition )
+    {
+      lcd.print("*");
+    }
+  }
+  else
+  {
+    if(_lockStatusToggle != _lockStatus)
+    {
+      cleared = 0;
+      statusChangeTime = millis();
+      lcd.setCursor(0, 0);
+      lcd.print("  VHOD V REZIDENCO  ");
+      lcd.setCursor(0,1);
+      lcd.print("     ODKLENJENO     ");
+      lcd.setCursor(0,3);
+      lcd.print("Geslo: ****");
+    }
+    if( (millis() - statusChangeTime > 1500) )
+    {
+      if( !cleared )
+      {
+        lcd.setCursor(0,3);
+        lcd.print("           ");
+        cleared = 1;
+      }
+      lcd.setCursor(0,3);
+      lcd.print("Zaklep cez "); // 9 characters remain for time XhXXmXXs
+      RemainingTime(AutoLock());
+      String toPrint;
+      toPrint = remainingTime[0] ? (String(remainingTime[0]) + "h") : ("");
+      toPrint += remainingTime[1] ? (String(remainingTime[1]) + "m") : ("");
+      toPrint += remainingTime[2] ? (String(remainingTime[2]) + "s") : ("0s");
+      while( toPrint.length() < 8)
+      {
+        toPrint = " " + toPrint;
+      }
+      lcd.print(toPrint);
+    }
+  }
+  
+  if( millis() - _lastKeyPressTime > AUTO_DISPLAY_OFF_TIME)
+  {
+    lcd.noBacklight();
+    lcd.noDisplay();
+  }
+  else
+  {
+    lcd.backlight();
+    lcd.display();
+  }
+
+  _enteredCodePositionPrev = _enteredCodePosition;
+  _lockStatusToggle = _lockStatus; 
+  
+}
+
+
+void RemainingTime(unsigned long millis)
+{
+  int inSeconds = millis / 1000;
+  remainingTime[0] = inSeconds / 3600;
+  remainingTime[1] = (inSeconds % 3600) / 60 ;
+  remainingTime[2] = ((inSeconds % 3600) % 60);
 }
