00694ec256a834182e2521beb8500c4d0cfad73d
LAPTOP\Nimfrodian <aljazpflaum@gmail.com>
2020-11-05 14:40:26 +0100

    Error on UNLOCKED_MENU

--- Door_with_latches/Door_with_latches.ino
+++ Door_with_latches/Door_with_latches.ino
@@ -2,11 +2,18 @@
 #include "Keypad.h"
 #include "Wire.h"
 #include "CapacitiveSensor.h"
+#include <EEPROM.h>
 
 typedef enum{
   LOCKED = 0,
   UNLOCKED
 } lockStatusType;
+
+typedef enum{
+  LOCKED_MENU = 0,
+  UNLOCKED_MENU,
+  CODE_CHANGE_MENU
+} menuType;
 
 /******* KEYPAD *******************************/
 const int ROW_NUM = 4; // four rows
@@ -26,6 +33,8 @@ byte pin_column[COLUMN_NUM] = {2, 3, 4, 5}; //connect to the column pinouts of t
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
 lockStatusType _lockStatus = LOCKED;
+menuType _menuType = LOCKED_MENU;
+menuType* _menuTypePtr = &_menuType;
 
 const unsigned long UNLOCK_ON_TIME = 3000;
 const unsigned long UNLOCK_LATCHES_MAX_TIME = 10000;
@@ -37,38 +46,40 @@ unsigned long AUTO_LOCK_TIME = 10000;
 unsigned long _lastUnlockTime = 0;
 unsigned long _lastKeyPressTime = 0;
 
-
 char _passCode[] = {'1', '8', '0', '9'};
 char _enteredCode[4] = {0};
 
 int remainingTime[3] = {0, 0, 0}; //hh mm ss
 
 int _enteredCodePosition = 0;
+int _numOfBpresses = 0;
 
 bool _unlockFlag = 0;
+bool _changeCodeFlag = 0;
 bool _lcdWrongCode = 0;
 
 /******* END_GLOBAL_VARIABLES *****************/
 /******* USED_PINS ****************************/
-int _latchGatePin = 12;
-int _insideSwitchPin = 11;
+int _latchGatePin = 12;     // pin for gate of MOSFET transistor for latches
+int _insideSwitchPin = 11;  // pin for handle capacitance
+int _closedDoorSensor = A0; // switch for monitoring if the doors are closed, 0 for CLOSED
+int _lockControlPin = A1;   // pin used for checking if the doors are actually locked
+int SCA_PIN = A4;
+int SCL_PIN = A5;
 CapacitiveSensor   _insideSwitchSensor = CapacitiveSensor(10,_insideSwitchPin);
 /******* END_USED_PINS ************************/
 /******* SETUP() ******************************/
 void setup() {
   pinMode( _latchGatePin, OUTPUT );
   digitalWrite( _latchGatePin, 0 );
+  pinMode( _closedDoorSensor, INPUT_PULLUP );
+  pinMode( _lockControlPin, INPUT_PULLUP );
   _insideSwitchSensor.set_CS_AutocaL_Millis(0xFFFFFFFF);
   Serial.begin(9600);
   Wire.begin();
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("  VHOD V REZIDENCO  ");
-  lcd.setCursor(0,1);
-  lcd.print("-----ZAKLENJENO-----");
-  lcd.setCursor(0,3);
-  lcd.print("Geslo: ");
+  setCodeFromEEPROM();
 }
 /******* END_SETUP() **************************/
 /******* MAIN() *******************************/
@@ -102,22 +113,30 @@ void loop() {
         break;
         case 'C':
         break;
-        case 'D':       
+        case 'D': 
         break;
-      break;
       }
+      _numOfBpresses = 0;
     }
     case UNLOCKED:
     {
+      static unsigned long _lastDefaultTime;
       switch(key)
       {
         case 'A':
         _unlockFlag = 1;
         break;
         case 'B':
+        _numOfBpresses++;
+        if(_numOfBpresses > 5)
+        {
+          _changeCodeFlag = 1;
+        }
+        break;
         break;
         case 'C':
         _lockStatus = LOCKED;
+        _menuType = LOCKED_MENU;
         AUTO_LOCK_TIME = AUTO_LOCK_TIME_RESET;
         ResetCode();
         break;
@@ -130,6 +149,8 @@ void loop() {
             AUTO_LOCK_TIME = AUTO_LOCK_TIME_MAX;
           }
         }        
+        break;
+        default:
         break;
       }
       break;
@@ -147,6 +168,8 @@ void loop() {
     if( ISSonTime - ISSoffTime < 200 )
     {
       _lockStatus = LOCKED;
+      ResetCode();
+      AUTO_LOCK_TIME = AUTO_LOCK_TIME_RESET;
     }
     ISSoffTime = millis();
   }
@@ -157,9 +180,10 @@ void loop() {
   
   
   UnlockLatches();
-  digitalWrite( 13, _lockStatus);
+  bool lockCheck = !digitalRead( _lockControlPin ); // 0 when switch closed, 1 when opened
+  digitalWrite( 13, _lockStatus && lockCheck);
   AutoLock();
-  printLCD();
+  printLCD(_menuTypePtr);
   if( Serial )
   {
     debug();
@@ -181,7 +205,7 @@ void CheckCode()
       {
         delay( 1000*attempts ); // progressively delay if attempts fail
       }
-      _lcdWrongCode = 1;
+      _menuType = LOCKED_MENU;
       return;
     }
   }
@@ -189,6 +213,7 @@ void CheckCode()
   _unlockFlag = 1;
   _lastUnlockTime = millis();
   _lockStatus = UNLOCKED;
+  _menuType = UNLOCKED_MENU;
 }
 
 void ResetCode()
@@ -210,7 +235,8 @@ bool UnlockLatches() // function for unlocking the latches and checking that pow
     _unlockFlag = 0;
   }
   bool timeCondition = (millis() - startTime) < UNLOCK_ON_TIME;
-  return PowerLimit(timeCondition);
+  bool doorState = !digitalRead( _closedDoorSensor );
+  return PowerLimit(timeCondition && doorState);
 }
 
 bool PowerLimit( bool condition)
@@ -257,6 +283,7 @@ unsigned long AutoLock() // locks the door after certain time has passed
   if( passedTime > AUTO_LOCK_TIME )
   {
     _lockStatus = LOCKED;
+    _menuType = LOCKED_MENU;
     AUTO_LOCK_TIME = AUTO_LOCK_TIME_RESET;
   }
   return AUTO_LOCK_TIME - passedTime;
@@ -272,6 +299,12 @@ void debug()
     Serial.print(String(_enteredCode[i]));
   }
   Serial.print("\t");
+  Serial.print("_passCode[]= ");
+  for( int i = 0; i < 4; i++)
+  {
+    Serial.print(String(_passCode[i]));
+  }
+  Serial.print("\t");
   unsigned long remainingTime = ((millis() - _lastUnlockTime) > AUTO_LOCK_TIME)? 0 : (AUTO_LOCK_TIME - (millis() - _lastUnlockTime));
   Serial.print("timeToLock: " + String(remainingTime) + "\t");
   Serial.println("");
@@ -282,96 +315,73 @@ void LOG(String parameter, String parameterName)
   Serial.print(parameterName + "= " + parameter + "\t");
 }
 
-void printLCD()
+void printLCD(menuType* menu)
 {
-  static bool _lockStatusToggle = 0;
-  static bool cleared = 0;
-  static int _enteredCodePositionPrev = 0;
-  static double long statusChangeTime = 0;
-
-  if( _lockStatus == LOCKED )
+  LOG(String(*menu), "Menu: ");
+  char toPrintLCD[4*20] = {"LCD error"};
+  switch( *menu )
   {
-    if(_lockStatusToggle != _lockStatus)
+    case LOCKED_MENU:
+    LOG("", "locked");
+    char lockedRow0[]= "  VHOD V REZIDENCO  ";
+    char lockedRow1[]= "-----ZAKLENJENO-----";
+    char lockedRow2[]= "                    ";
+    char lockedRow3[]= "Geslo:              ";
+    addToLCDtext(0, &toPrintLCD[0], &lockedRow0[0]);
+    addToLCDtext(1, &toPrintLCD[0], &lockedRow1[0]);
+    addToLCDtext(2, &toPrintLCD[0], &lockedRow2[0]);
+    addToLCDtext(3, &toPrintLCD[0], &lockedRow3[0]);
+    break;
+    case UNLOCKED_MENU:
+    LOG("", "unlocked");
+    char unlockedRow0[]= "  VHOD V REZIDENCO  ";
+    char unlockedRow1[]= "     ODKLENJENO     ";
+    char unlockedRow2[]= "                    ";
+    RemainingTime(AutoLock());
+    //char unlockedRow3[]= "                    ";
+    String unlockedRow3 = "Zaklep cez ";
+    String tempRow3 = remainingTime[0] ? (String(remainingTime[0]) + "h") : ("");
+    tempRow3 += remainingTime[1] ? (String(remainingTime[1]) + "m") : ("");
+    tempRow3 += remainingTime[2] ? (String(remainingTime[2]) + "s") : ("0s");
+    while( tempRow3.length() < 20 )
     {
-      statusChangeTime = millis();
-      lcd.setCursor(0, 0);
-      lcd.print("  VHOD V REZIDENCO  ");
-      lcd.setCursor(0,1);
-      lcd.print("-----ZAKLENJENO-----");
-      lcd.setCursor(0,3);
-      lcd.print("Geslo:              ");
+      tempRow3 = " " + tempRow3;
     }
-    if( _lcdWrongCode )
-    {
-      _lcdWrongCode = 0;
-      lcd.setCursor(0,3);
-      lcd.print("Geslo:              ");
-    }
-    lcd.setCursor(7,3);
-    for( int i = 0; i < 4; i++)
-    {
-      if( _enteredCodePosition > i) 
-      {
-        lcd.print("*");
-      }
-      else
-      {
-        lcd.print(" ");
-      }
-      
-    }
-
+    unlockedRow3 += tempRow3;
+    addToLCDtext(0, &toPrintLCD[0], &unlockedRow0[0]);
+    addToLCDtext(1, &toPrintLCD[0], &unlockedRow1[0]);
+    addToLCDtext(2, &toPrintLCD[0], &unlockedRow2[0]);
+    addToLCDtext(3, &toPrintLCD[0], &unlockedRow3[0]);
+    break;
+    case CODE_CHANGE_MENU:
+    LOG("", "changeMenu");
+    break;
+    default:
+    LOG("", "default");
+    break;
   }
-  else
-  {
-    if(_lockStatusToggle != _lockStatus)
-    {
-      cleared = 0;
-      statusChangeTime = millis();
-      lcd.setCursor(0, 0);
-      lcd.print("  VHOD V REZIDENCO  ");
-      lcd.setCursor(0,1);
-      lcd.print("     ODKLENJENO     ");
-      lcd.setCursor(0,3);
-      lcd.print("Geslo: ****");
-    }
-    if( (millis() - statusChangeTime > 1500) )
-    {
-      if( !cleared )
-      {
-        lcd.setCursor(0,3);
-        lcd.print("           ");
-        cleared = 1;
-      }
-      lcd.setCursor(0,3);
-      lcd.print("Zaklep cez "); // 9 characters remain for time XhXXmXXs
-      RemainingTime(AutoLock());
-      String toPrint;
-      toPrint = remainingTime[0] ? (String(remainingTime[0]) + "h") : ("");
-      toPrint += remainingTime[1] ? (String(remainingTime[1]) + "m") : ("");
-      toPrint += remainingTime[2] ? (String(remainingTime[2]) + "s") : ("0s");
-      while( toPrint.length() < 8)
-      {
-        toPrint = " " + toPrint;
-      }
-      lcd.print(toPrint);
-    }
-  }
-  
-  if( millis() - _lastKeyPressTime > AUTO_DISPLAY_OFF_TIME)
-  {
-    lcd.noBacklight();
-    lcd.noDisplay();
-  }
-  else
-  {
-    lcd.backlight();
-    lcd.display();
-  }
-  _lockStatusToggle = _lockStatus; 
-  
+  printToLCD(toPrintLCD);
 }
 
+void addToLCDtext( int line, char* toPrintArray, char* inputArray )
+{
+  for( int i = 0; i < 20; i++)
+  {
+    *(toPrintArray + i + line*20) = *(inputArray + i);
+  }
+}
+
+void printToLCD(char LCDarray[4*20])
+{
+  for( int i = 0; i < 4; i++)
+  {
+    lcd.setCursor(0,i);
+    for( int j = 0; j < (20); j++)
+    {
+      lcd.print(LCDarray[j+(20*i)]);
+    }
+  }
+}
 
 void RemainingTime(unsigned long millis)
 {
@@ -379,4 +389,23 @@ void RemainingTime(unsigned long millis)
   remainingTime[0] = inSeconds / 3600;
   remainingTime[1] = (inSeconds % 3600) / 60 ;
   remainingTime[2] = ((inSeconds % 3600) % 60);
+}
+
+void setCodeFromEEPROM()
+{
+  byte EEPROMcheck;
+  EEPROM.get(0, EEPROMcheck);
+  if( isDigit(EEPROMcheck) )
+  {
+    int address = 0;
+    for( int i = 0; i < 4; i ++)
+    {
+      EEPROM.get(i, _passCode[i]);
+    }
+  }
+}
+
+void putCodeToEEPROM()
+{
+  
 }
